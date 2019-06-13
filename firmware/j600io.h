83caa5d81988f61f2247188120b200ab2b3605da
Jean THOMAS <git0@pub.jeanthomas.me>
2019-06-13 21:38:46 +0200

    Update J600IO

--- firmware/j600io.h
+++ firmware/j600io.h
@@ -1,1 +1,6 @@
-void j600io_write_u600(uint16_t val);
+#include <generated/mem.h>
+
+#define J600IO_U600 *(volatile uint8_t*)(J600IO_BASE)
+#define J600IO_U601 *(volatile uint8_t*)(J600IO_BASE+4)
+#define J600IO_U604 *(volatile uint8_t*)(J600IO_BASE+8)
+#define J600IO_U605 *(volatile uint8_t*)(J600IO_BASE+12)
--- j600io.py
+++ j600io.py
@@ -5,36 +5,45 @@ class J600IO(Module):
 	def __init__(self, u600, u601, u604, u605, u608):
 		self.bus = bus = wishbone.Interface()
 
+		regbufs = {
+			"u600" : Signal(8),
+			"u601" : Signal(8),
+			"u604" : Signal(8),
+			"u605" : Signal(8),
+		}
+
 		self.sync += [
 			# DO NOT FORGET TO ACK <= '0' !!!!!!!! OTHERWISE THE CPU HANGS
 			bus.ack.eq(0),
 			If(bus.cyc & bus.stb & ~bus.ack,
 				If(bus.we,
-					#If(bus.adr & 0x7 == 0,
-						u600[0].eq(bus.dat_w[0]),
-						u600[1].eq(bus.dat_w[0]),
-						u600[2].eq(bus.dat_w[0]),
-						u600[3].eq(bus.dat_w[0]),
-						u600[4].eq(bus.dat_w[0]),
-						u600[5].eq(bus.dat_w[0]),
-						u600[6].eq(bus.dat_w[0]),
-						u600[7].eq(bus.dat_w[0]),
-						#u601.eq((bus.dat_w[0] >> 8) & 0xFF)
-					#).Elif(bus.adr & 0x7 == 1,
-						#u604.eq(bus.dat_w[0] & 0xFF),
-						#u605.eq((bus.dat_w[0] >> 8) & 0xFF)
-					#).Elif(bus.adr & 0x7 == 2,
-						#u605.eq(bus.dat_w[0] & 0xFF),
-					#)
+					If((bus.adr & 0x7) == 0,
+						regbufs["u600"].eq(bus.dat_w & 0xFF),
+					).Elif((bus.adr & 0x7) == 1,
+						regbufs["u601"].eq(bus.dat_w & 0xFF),
+					).Elif((bus.adr & 0x7) == 2,
+						regbufs["u604"].eq(bus.dat_w & 0xFF),
+					).Elif((bus.adr & 0x7) == 3,
+						regbufs["u605"].eq(bus.dat_w & 0xFF),
+					)
+				).Else(
+					If((bus.adr & 0x7) == 0,
+						bus.dat_r.eq(regbufs["u600"]),
+					).Elif((bus.adr & 0x7) == 1,
+						bus.dat_r.eq(regbufs["u601"]),
+					).Elif((bus.adr & 0x7) == 2,
+						bus.dat_r.eq(regbufs["u604"]),
+					).Elif((bus.adr & 0x7) == 3,
+						bus.dat_r.eq(regbufs["u605"]),
+					)
 				),
 				bus.ack.eq(1)
 			),
-			#u600[0].eq(1),
-			#u600[1].eq(1),
-			#u600[2].eq(1),
-			#u600[3].eq(1),
-			#u600[4].eq(1),
-			#u600[5].eq(1),
-			#u600[6].eq(1),
-			#u600[7].eq(1),
+		]
+
+		self.comb += [
+			u600.eq(regbufs["u600"]),
+			u601.eq(regbufs["u601"]),
+			u604.eq(regbufs["u604"]),
+			u605.eq(regbufs["u605"])
 		]
